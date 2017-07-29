4f7f6e7f9e51a0b37b3e537cc660b3aca770111c
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2017-07-29 11:56:15 +0700

    first commit

--- index.ios.js
+++ index.ios.js
@@ -17,14 +17,7 @@ export default class RMChatApp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Hello World
         </Text>
       </View>
     );
